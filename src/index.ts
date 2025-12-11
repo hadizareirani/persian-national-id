dbda1b69bff7e135f303370716747e82380dee93
Hadi Zare Irani <22741265+hadizareirani@users.noreply.github.com>
2025-12-11 07:20:15 +0330

    Update module imports to include file extensions in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
-export { generateNationalId } from "./modules/generateNationalId";
+export { generateNationalId } from "./modules/generateNationalId.js";
 
 /**
  * @deprecated Use `generateNationalId` instead. This alias will be removed in a future version.
  */
-export { generateNationalId as GenerateNationalId } from "./modules/generateNationalId";
+export { generateNationalId as GenerateNationalId } from "./modules/generateNationalId.js";
 
-export { validNationalId } from "./modules/validNationalId";
+export { validNationalId } from "./modules/validNationalId.js";
 
 /**
  * @deprecated Use `validNationalId` instead. This alias will be removed in a future version.
  */
-export { validNationalId as ValidNationalId } from "./modules/validNationalId";
+export { validNationalId as ValidNationalId } from "./modules/validNationalId.js";
